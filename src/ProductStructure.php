42ea42789af68cfd61b66fe03db3dbe22b6957ac
Jeferson Guedes <jguedes.tec@gmail.com>
2021-12-08 13:24:39 -0300

    init: create base structure

---
+++ src/ProductStructure.php
@@ -0,0 +1,26 @@
+<?php
+namespace App;
+
+class ProductStructure
+{
+    const products = [
+        "preto-PP",
+        "preto-M",
+        "preto-G",
+        "preto-GG",
+        "preto-GG",
+        "branco-PP",
+        "branco-G",
+        "vermelho-M",
+        "azul-XG",
+        "azul-XG",
+        "azul-XG",
+        "azul-P"
+    ];
+
+    public function make(): array
+    {
+       //todo your code.
+        return [];
+    }
+}
---
+++ test/ProductStructureTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use App\ProductStructure;
+
+class ProductStructureTest extends TestCase
+{
+    private ProductStructure $productStructure;
+
+    protected function setUp(): void
+    {
+        $this->productStructure = new ProductStructure();
+    }
+    public function testMake()
+    {
+        $this->assertEquals([
+            'preto' =>  [
+                'PP' => 1,
+                'M' => 1,
+                'G' => 1,
+                'GG' => 2
+            ],
+            'branco' => [
+                'PP'=> 1,
+                'G' => 1
+            ],
+            'vermelho' => [
+                'M' => 1
+            ],
+            'azul' => [
+                'XG' => 3,
+                'P' => 1
+            ]
+        ], $this->productStructure->make());
+
+    }
+}
